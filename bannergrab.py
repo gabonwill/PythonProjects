eb2bcc42845f347a24a46a52df2043caa9d820fa
Gbon <grss@Dacrib.localnet>
2019-04-26 03:00:05 -0400

    updates

--- bannergrab.py
+++ bannergrab.py
@@ -6,13 +6,13 @@ from multiprocessing import Process
 def connect():
     s = socket.socket()
     s.connect(("10.133.1.8", 53))
-    answer = s.recv(1024)
     f = open("index.html")
     data = f.read(1024)
     while(data):
         s.send(data)
         data = f.read(1024)
 
+    answer = s.recv(1024)
     print (answer)
     s.close()
 
